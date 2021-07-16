a1718927650e2ede229c1d191c75448f358d2144
gjc2010gys <gjc2010gys@sina.com>
2021-07-16 13:59:15 +0800

    新的更新

--- 新二元一次方程组GUI.py
+++ 新二元一次方程组GUI.py
@@ -1,5 +1,6 @@
-from os import system
+from os import system, terminal_size
 from random import randint
+from tkinter.constants import TRUE
 from easygui import msgbox,multenterbox,enterbox,ccbox
 
 def Make():
@@ -14,12 +15,24 @@ def Make():
     return x,y,fxbs,fybs,sxbs,sybs,fh,channe
 
 def Get():
-    global ix,iy
-    ix = OutList[0]
-    iy = OutList[1]
-    return ix,iy
+    global ix,iy,ClickCancel,NotType
+    ClickCancel = False
+    NotType = False
+    try:
+        ix = OutList[0]
+        iy = OutList[1]
+    except TypeError:
+        msgbox("别点Cancel！此题作废！")
+        ClickCancel = True
+        return ClickCancel
+    else:
+        if ix == '' or iy == '':
+            NotType = True
+            msgbox('你是不是有空没有输入？记得每个空一定都要输入，别空着，此题作废！')
+        return ix,iy,ClickCancel,NotType
 
-def CheckAnswer(ix,iy,x,y,channe):
+def CheckAnswer(ix,iy,x,y):
+    global channe
     if ix == str(x) and iy == str(y):
         channe -= 1
         msgbox('x正确.y正确.还需要答'+str(channe)+'次。',title='都正确！')
@@ -29,7 +42,6 @@ def CheckAnswer(ix,iy,x,y,channe):
         msgbox('y不正确,x正确。x为'+str(y)+'\n'+'\n'+'还需要答'+str(channe)+'次。',title='有错误！')
     else:
         msgbox('x和y都不正确。x为'+str(x)+','+'y为'+str(y)+'\n'+'\n'+'还需要答'+str(channe)+'次。',title='有错误！')
-    return channe
 
 def OutPut(x,y,fxbs,fybs,sxbs,sybs,fh):
     global FirstAnswer,SecondAnswer,OutList
@@ -65,17 +77,22 @@ if __name__ == '__main__':
     while Wrong:
         try:
             channe = int(enterbox(msg='应该答对多少题后结束呢？',title='请输入:'))
+        except TypeError:
+            msgbox(msg='别点Cancel！请重新输入！',title='输入错误！')
         except ValueError:
-            enterbox(msg='输入错误！你只能输入一个正整数！请重新输入！',title='输入错误！')
+            msgbox(msg='输入错误！你只能输入一个正整数！请重新输入！',title='输入错误！')
         else:
             Wrong = False
     while channe > 0:
         Make()
         OutPut(x,y,fxbs,fybs,sxbs,sybs,fh)
         Get()
-        CheckAnswer(ix,iy,x,y,channe)
+        if ClickCancel or NotType:
+            continue
+        CheckAnswer(ix,iy,x,y)
     
     if Close:
         msgbox("恭喜你答完了所有的题！正在恢复桌面。。。。。。")
+        system('C:/Windows//explorer.exe')
     else:
         msgbox('恭喜你答完了所有的题！正在退出。。。。。。')
